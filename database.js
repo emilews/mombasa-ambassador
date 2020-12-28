61d523a4a13715dfc5466498a372c4999a22bf12
Emilio Wong <emilews.ew@gmail.com>
2020-12-27 20:17:10 -0700

    Changed switch statements for if, saving data now uses latest BCH price and added some logs

--- database.js
+++ database.js
@@ -1,39 +1,71 @@
 const mongoose = require('mongoose');
-import { Crypto, Fiat } from './models';
-import { TransformPriceToBCH } from './utils';
-
-mongoose.connect('mongodb://localhost/mombasa', {useNewUrlParser: true});
+const { Crypto, Fiat } = require('./models');
+const { GetCachedBCHPrice } = require('./utils');
+mongoose.connect('mongodb://localhost/mombasa', {useNewUrlParser: true,  useUnifiedTopology: true});
 
 const SaveData = (data) => (kind) => {
-    switch (kind){
-        case 'fiat':
-            let fiat = new Fiat({fiat_ticker: data.ticker, price: data.price});
-            fiat.save();
-        case 'crypto':
-            let crypto = new Crypto({ticker: data.ticker, last_update: new Date().toISOString(), bch_price: (data.ticker === "BCH" ? 1 : TransformPriceToBCH(data.price)), usd_price: data.price});
-            crypto.save();
+    if(kind === 'fiat') {
+        Fiat.exists({ fiat_ticker: data.ticker}, (err, doc) => {
+            if(err){
+                console.log(err);
+            }else{
+                if(doc){
+                    Fiat.updateOne({fiat_ticker: data.ticker}, { $set: { price: data.price }}, {multi: false}, (err, numAffected) => {
+                        if(err){
+                            console.log(err);
+                        }else{
+                            console.log(`Found ${numAffected.n} fiat records. With ${numAffected.nModified} affected.`)
+                        }
+                    });
+                }else{
+                    let fiat = new Fiat({fiat_ticker: data.ticker, price: data.price});
+                    fiat.save();
+                }
+            }
+        })
+    }else{
+        Crypto.exists({ ticker: data.ticker}, (err, doc) => {
+            if(err){
+                console.log(err);
+            }else{
+                if(doc){
+                    Crypto.updateOne({ticker: data.ticker}, { $set: { last_update: new Date().toISOString(), bch_price: (data.ticker === "BCH" ? 1 : data.price/GetCachedBCHPrice()), usd_price: data.price }}, {multi: false}, (err, numAffected) => {
+                        if(err){
+                            console.log(err);
+                        }else{
+                            console.log(`Found ${data.ticker} crypto record. New price: ${data.price}`)
+                        }
+                    });
+                }else{
+                    let crypto = new Crypto({ticker: data.ticker, last_update: new Date().toISOString(), bch_price: (data.ticker === "BCH" ? 1 : data.price/GetCachedBCHPrice()), usd_price: data.price});
+                    crypto.save();
+                }
+            }
+        })
     }
 }
 
-export const GetDatabaseInfo = (data) => (kind) => {
-    switch (kind){
-        case 'fiat':
-            Fiat.find({fiat_ticker: data.fiat_ticker }, (err, fiat) => {
-                if (err) {
-                    return null;
-                } else {
-                    return fiat;
-                }
-            });
-        case 'crypto':
-            Crypto.find({ticker: data.ticker }, (err, crypto) => {
-                if (err) {
-                    return null;
-                } else {
-                    return crypto;
-                }
-            });
-        default:
-            //nothing
+const GetDatabaseInfo = (data) => (kind) => {
+    let res;
+    if(kind === 'fiat') {
+        Fiat.findOne({fiat_ticker: data.ticker }, function (err, fiat){
+            res = fiat;
+        });
+    }else {
+        Crypto.findOne({ticker: data.ticker }, function (err, crypto){
+            if(err){
+                console.log(err)
+            }else{
+                res = crypto;
+            }
+        });
     }
 }
+
+const TransformPriceToBCH = async (usd_price) =>{
+    let bch_info = GetDatabaseInfo({ticker: "BCH"})("crypto");
+    return usd_price/bch_info.usd_price;
+}
+
+
+module.exports = { GetDatabaseInfo, SaveData}

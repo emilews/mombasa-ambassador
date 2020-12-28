bc0bb08a784d8de3743c81b4d59feda250c8a025
Emilio Wong <emilews.ew@gmail.com>
2020-12-27 20:22:00 -0700

    Moved 'TransformPriceToBCH' to database and added latest price for BCH

--- utils.js
+++ utils.js
@@ -1,6 +1,10 @@
-import { GetDatabaseInfo } from './database';
 
-export const TransformPriceToBCH = (usd_price) =>{
-    let bch_data = GetDatabaseInfo({ticker: "BCH"})("crypto");
-    return (usd_price)/bch_data.usd_price;
+let BCH_PRICE = 0;
+const GetCachedBCHPrice = () => {
+    return BCH_PRICE;
 }
+const SetCachedBCHPrice = (price) => {
+    BCH_PRICE = price;
+}
+
+module.exports = { SetCachedBCHPrice, GetCachedBCHPrice };

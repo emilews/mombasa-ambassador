b5a11f55ea662ba7d2134f446021a358b96dc042
Emilio Wong <emilews.ew@gmail.com>
2020-12-27 20:20:30 -0700

    Refactored

--- models.js
+++ models.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 
-export const Crypto = mongoose.model('Crypto', { ticker: String, last_update: String, bch_price: Number, usd_price: Number });
+const Crypto = mongoose.model('Crypto', { ticker: String, last_update: String, bch_price: Number, usd_price: Number });
 
-export const Fiat = mongoose.model('Fiat', { fiat_ticker: String, price: Number});
+const Fiat = mongoose.model('Fiat', { fiat_ticker: String, price: Number});
+
+module.exports = { Crypto, Fiat };

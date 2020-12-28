d7603277f0748326c3c7d0c9c59ede63e76fe40b
Emilio Wong <emilews.ew@gmail.com>
2020-12-27 20:21:12 -0700

    Refactored and added more tags for the lookup

--- parser.js
+++ parser.js
@@ -1,23 +1,31 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-var cachedCryptoPage = null;
-var cachedFiatPage = null;
 const CRYPTO_PRICE_ELEMENT_TAG = 'cmc-details-panel-price__price';
-const FIAT_PRICE_ELEMENT_TAG = 'converterresult-toAmount';
+const FIAT_PRICE_ELEMENT_TAG = 'intraday__price';
 
-export const ParseCryptoPage = (page) => {
-    cachedCryptoPage = new JSDOM(page);
+const GetCryptoPriceElement = (page) => {
+    const cryptoPage = new JSDOM(page.body);
+
+    if(cryptoPage.window.document.querySelector(`span.${CRYPTO_PRICE_ELEMENT_TAG}`) !== null){
+        return parseFloat(cryptoPage.window.document.querySelector(`span.${CRYPTO_PRICE_ELEMENT_TAG}`).textContent.split("$")[1].replace(/,/g, ''))
+    }else {
+        if(cryptoPage.window.document.querySelector(`span.${CRYPTO_PRICE_ELEMENT_TAG}.cmc-detail-price-down-active`) !== null){
+            return parseFloat(cryptoPage.window.document.querySelector(`span.${CRYPTO_PRICE_ELEMENT_TAG}.cmc-detail-price-down-active`).textContent.split("$")[1].replace(/,/g, ''));
+        }else{
+            if(cryptoPage.window.document.querySelector(`span.${CRYPTO_PRICE_ELEMENT_TAG}.cmc-detail-price-up-active`) !== null){
+                return parseFloat(cryptoPage.window.document.querySelector(`span.${CRYPTO_PRICE_ELEMENT_TAG}.cmc-detail-price-up-active`).textContent.split("$")[1].replace(/,/g, ''));
+            }
+        }
+    }
 }
 
-export const GetCryptoPriceElement = () => {
-    return parseFloat(cachedCryptoPage.window.document.querySelector(`span.${CRYPTO_PRICE_ELEMENT_TAG}`).textContent);
+const GetFiatPriceElement = (page) => {
+    const fiatPage = new JSDOM(page.body);
+    const data = fiatPage.window.document.querySelector(`h3.${FIAT_PRICE_ELEMENT_TAG}`).textContent.toString().split(" ").filter((item) => {
+        return item.search(new RegExp('[0-9]')) === 0;
+    });
+    return data[0];
 }
 
-export const ParseFiatPage = (page) => {
-    cachedFiatPage = new JSDOM(page);
-}
-
-export const GetFiatPriceElement = () => {
-    return parseFloat(cachedPage.window.document.querySelector(`span.${FIAT_PRICE_ELEMENT_TAG}`).textContent);
-}
+module.exports = { GetCryptoPriceElement, GetFiatPriceElement };
